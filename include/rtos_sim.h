0be2b3fcd019db0296bdcb2e25b5dfc37c60dfcb
Maximilian Feldthusen <96908021+maximilianfeldthusen@users.noreply.github.com>
2026-04-14 09:18:53 +0200

    Refactor rtos_sim.h for task management
    Removed rollback protection definitions and added RTOS task management functions.

--- include/rtos_sim.h
+++ include/rtos_sim.h
@@ -19,37 +19,15 @@
  */
 
 
-#ifndef ROLLBACK_PROTECTION_H
-#define ROLLBACK_PROTECTION_H
+#ifndef RTOS_SIM_H
+#define RTOS_SIM_H
 
 #include <stdint.h>
-#include <stddef.h>
-#include "config.h"
 
-#ifdef __cplusplus
-extern "C" {
+typedef void *(*task_fn_t)(void *);
+
+int rtos_create_task(task_fn_t fn, const char *name, void *arg);
+void rtos_delay(uint32_t ms);
+
 #endif
-
-typedef struct {
-    uint32_t version_counter;
-    uint8_t signature[64];
-    uint64_t last_updated;
-    uint8_t flags;
-    uint8_t reserved[3];
-} rollback_protection_t;
-
-int rollback_init(rollback_protection_t *rb);
-int rollback_check_and_increment(rollback_protection_t *rb,
-                                uint32_t min_version);
-int rollback_get_current_version(rollback_protection_t *rb,
-                                uint32_t *version);
-int rollback_verify_signature(rollback_protection_t *rb);
-int rollback_sign(rollback_protection_t *rb);
-void rollback_cleanup(rollback_protection_t *rb);
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif /* ROLLBACK_PROTECTION_H */
 
